5d2be70d326db9b1710aed127a0876c24cbcba7d
wongyujing <wongyujing@163.com>
2023-02-03 01:04:04 +0800

    feat: 11

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,13 @@
-import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
-import './index.less'
+import { View } from '@tarojs/components'
+import './index.less';
 
-export default class Index extends Component<PropsWithChildren> {
+definePageConfig({
+  navigationStyle: 'custom',
+});
 
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
-    return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-      </View>
-    )
-  }
+export default function Home() {
+  return (
+    <View>hello world</View>
+  )
 }
+
