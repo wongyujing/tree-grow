40a41c5bdda218a6ab6bc607ec99424f1c5319da
wongyujing <wongyujing@163.com>
2023-02-08 23:01:18 +0800

    feat: home

---
+++ src/pages/index/components/timing/index.tsx
@@ -0,0 +1,12 @@
+import { memo } from 'react';
+import { View } from '@tarojs/components';
+
+const Timing = () => {
+  return (
+    <View>
+      <View></View>
+    </View>
+  )
+}
+
+export default memo(Timing);
